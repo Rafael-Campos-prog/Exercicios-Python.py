99394929fe6cd1b93b4d992b610d729d2fbd47e8
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2021-09-07 21:06:54 -0300

    Introdução a listas.

--- exe077.py
+++ exe077.py
@@ -2,7 +2,7 @@ palavras =('aprender', 'programar', 'linguagem', 'python',
            'curso', 'gratis', 'estudar', 'praticar',
            'trabalhar', 'mercado', 'programador', 'futuro')
 for p in palavras:
-    print(f'\nNa palavra\033[32m{p.upper()}\033[m temos ', end='')
+    print(f'\nNa palavra\033[36m{p.upper()}\033[m temos ', end='')
     for letra in p:
         if letra.lower() in 'aeiou':
             print(letra, end=' ')
