1fd56b32d7793f8fa7ea91d98426a92789ec2a5a
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-01-02 12:59:54 -0300

    Programa de cadastro de 7 números em uma lista que separa os números pares dos ímpares emostra em ordem crescente.

--- exe085.py
+++ exe085.py
@@ -1,7 +1,7 @@
 num = [[], []]
 valor = 0
 for c in range(1, 8):
-    valor = int(input(f'Digite {c}º valor: '))
+    valor = int(input(f'Digite o {c}º valor: '))
     if valor % 2 ==0:
         num[0].append(valor)
     else:
