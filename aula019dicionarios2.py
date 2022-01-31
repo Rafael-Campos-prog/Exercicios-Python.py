349a411ba6486ff9a80df102bcd253eed2bd6ebf
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-01-31 06:57:50 -0300

    Aprendendo dicionários em python com  exemplos de estados brasileiros.

---
+++ aula019dicionarios2.py
@@ -0,0 +1,23 @@
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro','sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla' : 'SP'}
+
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(estado2)
+print(brasil)
+print(brasil[0]['uf'])
+print(brasil[1]['sigla'])
+
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Fderativa: '))
+    estado['sigla'] = str(input('Sigla do estado: '))
+ #fatiamento em dicionário com método interno (copy) cria relação entre dicionários e listas.
+    brasil.append(estado.copy())
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
