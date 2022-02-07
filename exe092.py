84c6796adf8c33d951ea67e8e7ba7a96771124a1
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-02-07 11:48:41 -0300

    Cadastro do trabalhador que guarda nome, ano de nascimento e carteira de trabalho (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário recebe também o ano de contratação e o salário. Calcula e acrescenta, além da idade, com quantos anos a pessoa vai se aposentar.

---
+++ exe092.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from time import sleep
+dados = dict()
+dados['Nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+dados['Idade'] = datetime.now().year - nasc
+dados['Ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['Ctps'] != 0:
+    dados['Contratação'] = int(input('Ano de contratação: '))
+    dados['Salário'] = float(input('Salário: R$'))
+    dados['Aposentadoria'] = dados['Idade'] + ((dados['Contratação'] + 35) - datetime.now().year)
+print('<->' * 20)
+for k, v in dados.items():
+    print(f'  - {k}  {v}')
+    sleep(1)
