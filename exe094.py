aa139fcadba61052e281fd1d7fea9b344411cb00
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-02-15 06:41:44 -0300

    Programa que cadastra nome , sexo,e idade de várias pessoas em um dicionário e os dicionários em uma lista e mostra quantas pessoas foram cadastradas, a média de idade, uma lista com as mulheres e uma lista com idades acima da média.

---
+++ exe094.py
@@ -0,0 +1,47 @@
+from time import sleep
+galera = list()
+pessoa = dict()
+soma = média = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
+        break
+print('<->' * 25)
+sleep(1.5)
+print(f'Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print('-=' * 45)
+sleep(1.5)
+print(f'A média de idade é de {média:5.2f} anos')
+print('-=' * 45)
+sleep(2)
+print(f' As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}, ', end='')
+print()
+print('-=' * 45)
+sleep(2.5)
+print(' Lista de pessoas que estão acima da média: ', end='')
+for p in galera:
+    if p['idade'] >= média:
+        print('  ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+sleep(2)
+print('<<ENCERRADO>>')
