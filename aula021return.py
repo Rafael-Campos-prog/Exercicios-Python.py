1065e4efef59dcfc64942720c0c8a2690942f1f8
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-02-21 06:46:52 -0300

    Aula 21 Funções:Retorno de resultados.

---
+++ aula021return.py
@@ -0,0 +1,42 @@
+# Retorno de resultados
+
+
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    return s
+
+
+r1 = somar(3, 2, 5)
+r2 = somar(2, 5)
+r3 = somar(6)
+
+print(f'Os resultados foram {r1} , {r2}, {r3}.')
+
+
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+n = int(input('Digite um número: '))
+print(fatorial(n))
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}.')
+
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('É ímpar!')
