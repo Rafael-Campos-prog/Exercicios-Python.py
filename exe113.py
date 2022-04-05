43545a5142297d609f83a275b29b769f9b45086a
RafaelCampos <75264945+Rafael-Campos-prog@users.noreply.github.com>
2022-04-04 22:29:40 -0300

    Função leiaInt() e função leiaFloat() com possibilidade de digitação de um número de tipo inválido.(Tratamneto de erros e exceções).

---
+++ exe113.py
@@ -0,0 +1,33 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário\033[m')
+            return 0
+        else:
+            return n
+
+
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except(ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número real válido.\033[m')
+            continue
+        except(KeyboardInterrupt):
+            print('\033[31mEntrada de dados interrom pida pelo usuário.\033[m')
+            return 0
+        else:
+            return n
+
+
+n1 = leiaInt('Digite um número inteiro: ')
+n2 = leiaFloat('Digite um número real: ')
+print(f'O número inteiro digitado foi {n1} e o número real digitado foi {n2}.')
